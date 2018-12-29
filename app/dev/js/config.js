bd78131dd9af3eaf36603eb39b6aca00a8bd8fa5
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2018-12-29 16:22:05 +0530

    video update, animations synced accordingly

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -7,7 +7,7 @@ var config = {
     {
       class: 'charlie animate__name',
       animations: 'animation-start, animation-end',
-      times: '0.3, 2.3',
+      times: '0.3, 3.2',
     },
     {
       tag: 'span',
@@ -23,32 +23,32 @@ var config = {
     {
       class: 'charlie animate2__line1',
       animations: 'animation2-start, animation2-end',
-      times: '11, 12.8',
+      times: '15, 17.2',
       text: 'with Loan against your',
     },{
       class: 'charlie animate2__line2',
       animations: 'animation2-start, animation2-end',
-      times: '11.1, 12.8',
+      times: '15.1, 17.2',
       text: 'HDFC Bank',
     },{
       class: 'charlie animate2__line3',
       animations: 'animation2-start, animation2-end',
-      times: '11.2, 12.8',
+      times: '15.2, 17.2',
       target: 'cardType',
     },{
       class: 'charlie animate2__line4',
       animations: 'animation2-start, animation2-end',
-      times: '11.3, 12.8',
+      times: '15.3, 17.2',
       text: 'Credit Card',
     },{
       class: 'charlie animate2__line5',
       animations: 'animation2-start, animation2-end',
-      times: '11.4, 12.8',
+      times: '15.4, 17.2',
       target: 'cardNumber',
     },{
       class: 'charlie animate3 show-end',
       animations: 'animation-start',
-      times: '15.4'
+      times: '20.5'
     },
     {
       tag: 'span',
@@ -68,7 +68,7 @@ var config = {
       tag: 'a',
       class: 'charlie animate-btn show-end',
       animations: 'animation-start',
-      times: '15.4',
+      times: '20.5',
       href: 'https://leads.hdfcbank.com/applications/webforms/apply/HDFC_InstaLoan_LandingPage/index.aspx?PromoCode=sms-lacc-insta&utm_tag=know_more&utm_campaign=lacc-insta&utm_medium=cpm&utm_source=facebook&utm_content=old-carousel1',
       newWindow: 'true'
     }
