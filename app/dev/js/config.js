afb32e275d9f14de3a9afc5906be8aad49e6b963
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2018-12-29 13:23:09 +0530

    hdfc-ny-loan-cc init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,13 +1,13 @@
 var config = {
   // config here
-  videoUrl: './app/img/christmas-loan-cc-empty.mp4',
+  videoUrl: './app/img/ny-loan-cc-empty.mp4',
   startPoster: './app/img/startPoster.jpg',
   endPoster: './app/img/endPoster.jpg',
   rules: [
     {
       class: 'charlie animate__name',
       animations: 'animation-start, animation-end',
-      times: '0.3, 3.3',
+      times: '0.3, 2.3',
     },
     {
       tag: 'span',
@@ -23,32 +23,32 @@ var config = {
     {
       class: 'charlie animate2__line1',
       animations: 'animation2-start, animation2-end',
-      times: '16, 18.4',
+      times: '11, 12.8',
       text: 'with Loan against your',
     },{
       class: 'charlie animate2__line2',
       animations: 'animation2-start, animation2-end',
-      times: '16.1, 18.4',
+      times: '11.1, 12.8',
       text: 'HDFC Bank',
     },{
       class: 'charlie animate2__line3',
       animations: 'animation2-start, animation2-end',
-      times: '16.2, 18.4',
+      times: '11.2, 12.8',
       target: 'cardType',
     },{
       class: 'charlie animate2__line4',
       animations: 'animation2-start, animation2-end',
-      times: '16.3, 18.4',
+      times: '11.3, 12.8',
       text: 'Credit Card',
     },{
       class: 'charlie animate2__line5',
       animations: 'animation2-start, animation2-end',
-      times: '16.4, 18.4',
+      times: '11.4, 12.8',
       target: 'cardNumber',
     },{
       class: 'charlie animate3 show-end',
       animations: 'animation-start',
-      times: '22.1'
+      times: '15.4'
     },
     {
       tag: 'span',
@@ -68,7 +68,7 @@ var config = {
       tag: 'a',
       class: 'charlie animate-btn show-end',
       animations: 'animation-start',
-      times: '22.1',
+      times: '15.4',
       href: 'https://leads.hdfcbank.com/applications/webforms/apply/HDFC_InstaLoan_LandingPage/index.aspx?PromoCode=sms-lacc-insta&utm_tag=know_more&utm_campaign=lacc-insta&utm_medium=cpm&utm_source=facebook&utm_content=old-carousel1',
       newWindow: 'true'
     }
