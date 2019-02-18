ae9eba288e7b14e44a31362d2bb43adad9e543eb
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-02-18 12:22:47 +0530

    pnb-metlife init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,26 @@
 var config = {
   // config here
+  videoUrl: "./app/img/pnb-metlife-empty.mp4",
+  startPoster: "./app/img/startPoster.jpg",
+  endPoster: "./app/img/endPoster.jpg",
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: ".9"
+    },
+    {
+      tag: "span",
+      text: "Dear ",
+      parent: "#name"
+    },
+    {
+      tag: "span",
+      target: "name",
+      parent: "#name"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
