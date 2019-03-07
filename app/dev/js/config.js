c78072caedbb2ecdfa56f49eaee28635f4b3cd36
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-03-07 17:45:46 +0530

    hdfc-skoda-rapid init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,47 @@
 var config = {
   // config here
+  videoUrl: "./app/img/skoda-rapid-empty.mp4",
+  startPoster: "./app/img/startPoster.jpg",
+  endPoster: "./app/img/endPoster.jpg",
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: "1.8",
+      target: "name"
+    },
+    {
+      id: "loan",
+      class: "charlie show-end",
+      animations: "animate-loan",
+      times: "47.5"
+    },
+    {
+      tag: "span",
+      text: "₹",
+      parent: "#loan"
+    },
+    {
+      tag: "span",
+      target: "loan",
+      parent: "#loan"
+    },
+    {
+      tag: "span",
+      text: " lacs",
+      parent: "#loan"
+    },
+    {
+      tag: "a",
+      id: "link",
+      class: "charlie show-end",
+      animations: "animate-link",
+      times: "47.8",
+      hrefTarget: "link",
+      newWindow: "true"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
