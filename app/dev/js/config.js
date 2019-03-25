825c9c1b61ec5c64cb3e22dc4541d122783c89ab
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-03-25 23:39:07 +0530

    hdfc-birthday init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,23 @@
 var config = {
   // config here
+  videoUrl: "./app/img/birthday-empty.mp4",
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: "2.3"
+    },
+    {
+      id: "greeting",
+      text: "Dear",
+      parent: "#name"
+    },
+    {
+      target: "name",
+      parent: "#name"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
