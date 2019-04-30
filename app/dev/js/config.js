b2d1b799d586f5f107e00779de6b7ef6ae147a2a
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-04-30 13:21:04 +0530

    malabar-gold-promise init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,25 @@
 var config = {
   // config here
+  videoUrl: './app/img/gold-promise-empty.mp4',
+  startPoster: './app/img/startPoster.jpg',
+  endPoster: './app/img/endPoster.jpg',
+  rules: [
+    {
+      id: "greeting",
+      class: "charlie",
+      animations: "animate-name",
+      times: "1.5",
+      text: "Dear"
+    },
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: "1.5",
+      target: "name",
+      text: ","
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
