d3f6fb49c33f28d86e14b40f742e64c00f1df0a4
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-06-03 12:24:35 +0530

    hdfc-digital-loan-mf init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,35 @@
 var config = {
   // config here
+  videoUrl: './app/img/digital-loan-mf-empty.mp4',
+  startPoster: './app/img/startPoster.jpg',
+  endPoster: './app/img/endPoster.jpg',
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: ".1"
+    },
+    {
+      tag: "span",
+      text: "Dear ",
+      parent: "#name"
+    },
+    {
+      tag: "span",
+      target: "name",
+      parent: "#name"
+    },
+    {
+      tag: "a",
+      id: "link",
+      class: "charlie show-end",
+      animations: "animate-link",
+      times: "55",
+      hrefTarget: "link",
+      newWindow: "true"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
