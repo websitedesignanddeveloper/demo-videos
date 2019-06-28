6c9018d213cb84bf4e3d0e98681ff5a75bf51ead
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-06-28 12:46:16 +0530

    hdfc-cc-worldcup init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,91 @@
 var config = {
   // config here
+  videoUrl: "./app/img/cc-worldcup-empty.mp4",
+  startPoster: "./app/img/startPoster.jpg",
+  endPoster: "./app/img/endPoster.jpg",
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: ".1"
+    },
+    {
+      tag: "span",
+      text: "Hey ",
+      parent: "#name"
+    },
+    {
+      tag: "span",
+      target: "name",
+      parent: "#name"
+    },
+    {
+      tag: "span",
+      text: ",",
+      parent: "#name"
+    },
+    {
+      id: "text1",
+      class: "charlie",
+      animations: "animate-name",
+      text: "You're getting closer to",
+      times: ".1"
+    },
+    {
+      id: "text2",
+      class: "charlie",
+      animations: "animate-name",
+      text: "the Holiday to England with",
+      times: ".1"
+    },
+    {
+      id: "card",
+      class: "charlie",
+      animations: "animate-name",
+      times: ".1"
+    },
+    {
+      tag: "span",
+      text: "your ",
+      parent: "#card"
+    },
+    {
+      tag: "span",
+      target: "card",
+      parent: "#card"
+    },
+    {
+      tag: "span",
+      text: " Card.",
+      parent: "#card"
+    },
+    {
+      id: "runs",
+      class: "charlie",
+      animations: "animate-runs",
+      times: "4.5"
+    },
+    {
+      tag: "span",
+      target: "runs",
+      parent: "#runs"
+    },
+    {
+      tag: "span",
+      text: " runs",
+      parent: "#runs"
+    },
+    {
+      tag: "a",
+      id: "link",
+      class: "charlie",
+      animations: "animate-link",
+      times: "19",
+      hrefTarget: "link",
+      newWindow: "true"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
