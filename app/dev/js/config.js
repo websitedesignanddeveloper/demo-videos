c18109a695546e04f169c221ecd3174edfc0aa50
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-06-30 01:07:55 +0530

    start and end poster added

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,6 +1,8 @@
 var config = {
   // config here
   videoUrl: './app/img/dental-clinic30.mp4',
+  startPoster: './app/img/startPoster.jpg',
+  endPoster: './app/img/endPoster.jpg',
   rules: []
 };
 var pictor = new Pictor(config);
