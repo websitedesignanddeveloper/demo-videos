cbccb28e320341eaf1c16b015a17fef37658ae1b
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-07-09 22:15:12 +0530

    hdfc-regalia-09072019 init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,10 +1,52 @@
 var config = {
   // config here
+  videoUrl: './app/img/regalia.mp4',
+  startPoster: './app/img/startPoster.jpg',
+  endPoster: './app/img/endPoster.jpg',
+  rules: [
+    {
+      tag: "a",
+      id: "knowMore",
+      class: "charlie",
+      animations: "animate-knowMore",
+      times: "32",
+      hrefTarget: "knowMore",
+      newWindow: "true"
+    },
+    {
+      tag: "a",
+      id: "availNow",
+      class: "charlie",
+      animations: "animate-availNow",
+      times: "58.5",
+      hrefTarget: "availNow",
+      newWindow: "true"
+    },
+    {
+      tag: "a",
+      id: "pinGenerate",
+      class: "charlie",
+      animations: "animate-pinGenerate",
+      times: "74",
+      hrefTarget: "pinGenerate",
+      newWindow: "true"
+    },
+    {
+      tag: "a",
+      id: "knowMore1",
+      class: "charlie show-end",
+      animations: "animate-knowMore1",
+      times: "87.5",
+      hrefTarget: "knowMore",
+      newWindow: "true"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
 pictor.init();
 
-// $(document).on('pictorInit', function() {
-//   // project specific functions should here
-// });
+$(document).on('pictorInit', function() {
+  // project specific functions should here
+  $('#pageBtn').attr('href',pictor.data.availNow);
+});
