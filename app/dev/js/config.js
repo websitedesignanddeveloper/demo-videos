257faac27da7f747d9bea298a64e5ab895fcf875
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-07-13 10:09:50 +0530

    hdfc-loan-against-securities init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,9 @@
 var config = {
   // config here
+  videoUrl: './app/img/loan-against-securities.mp4',
+  startPoster: './app/img/startPoster.jpg',
+  endPoster: './app/img/endPoster.jpg',
+  rules: []
 };
 var pictor = new Pictor(config);
 
