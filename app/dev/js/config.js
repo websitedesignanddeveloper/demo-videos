a63e81f2aa4fd4fe6d9b7aea32c02269832c3af7
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-07-22 11:45:01 +0530

    hdfc-vrm init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -4,159 +4,159 @@ var config = {
   startTarget: "startPoster",
   endTarget: "endPoster",
   rules: [
-    {
-      id: "name",
-      class: "charlie",
-      animations: "animate-name",
-      times: ".5"
-    },
-    {
-      tag: "span",
-      text: "Dear ",
-      parent: "#name"
-    },
-    {
-      tag: "span",
-      target: "name",
-      parent: "#name"
-    },
-    {
-      tag: 'div',
-      class: 'animate2__text',
-      parent: '#textAnimationBlock'
-    },{
-      tag: 'div',
-      id: 'animate2__text-line1',
-      class: 'charlie display-none',
-      parent: '.animate2__text',
-      animations: 'animate-start,animate-finish',
-      times: '5,8.6',
-      text: 'custId'
-    },{
-      tag: 'div',
-      id: 'animate2__text-line2',
-      class: 'charlie display-none',
-      parent: '.animate2__text',
-      animations: 'animate-start,animate-finish',
-      times: '5.2,8.6',
-      text: 'custId'
-    },{
-      tag: 'div',
-      id: 'animate2__text-line3',
-      class: 'charlie display-none',
-      parent: '.animate2__text',
-      animations: 'animate-start,animate-finish',
-      times: '5.4,8.6',
-      text: 'custId'
-    },{
-      tag: 'div',
-      id: 'animate2__text-line4',
-      class: 'charlie',
-      parent: '.animate2__text',
-      animations: 'animate-start,animate-finish',
-      times: '5.6,8.6',
-      target: 'custId'
-    },
-    {
-      tag: 'div',
-      class: 'animate3',
-      parent: '#textAnimationBlock'
-    },
-    {
-      tag: 'div',
-      class: 'charlie',
-      parent: '.animate3',
-      // animations: 'animate3__card1, animate3__card2 ,animate3-finish',
-      animations: 'animate3__card2 ,animate3-finish',
-      id: 'animate3__card',
-      // times: '9.7, 11,17.5'
-      times: '11,17.5'
-    },
-    {
-      tag: 'div',
-      class: 'card__info',
-      parent: '#animate3__card'
-    },
-    {
-      tag: 'div',
-      class: 'info__name',
-      parent: '.card__info',
-      target: 'card1Name'
-    },
-    {
-      tag: 'div',
-      class: 'info__position',
-      parent: '.card__info'
-    },
-    {
-      tag: 'span',
-      text: 'Your ',
-      parent: '.info__position'
-    },
-    {
-      tag: 'span',
-      target: 'card1Position',
-      parent: '.info__position'
-    },
-    {
-      tag: 'div',
-      class: 'info__number',
-      parent: '.card__info',
-      target: 'card1Number'
-    },
-    {
-      tag: 'div',
-      id: 'animate__line',
-      class: 'charlie',
-      animations: 'animate-start,animate-finish',
-      times: '17.2,22.1'
-    },
-    {
-      tag: 'span',
-      text: 'In case you are unable to reach ',
-      parent: '#animate__line'
-    },
-    {
-      tag: 'span',
-      target: 'card1Firstname',
-      parent: '#animate__line'
-    },
-    {
-      tag: 'div',
-      class: 'animate4',
-      parent: '#textAnimationBlock'
-    },
-    {
-      tag: 'div',
-      id: 'animate4__name',
-      class: 'charlie',
-      parent: '.animate4',
-      animations: 'animate-start,animate-finish',
-      times: '18.7,22.1',
-      target:'card2Name'
-    },
-    {
-      tag: 'div',
-      id: 'animate4__number',
-      class: 'charlie',
-      parent: '.animate4',
-      animations: 'animate-start,animate-finish',
-      times: '19.3,22.1',
-      target:'card2Number'
-    },
+    // {
+    //   id: "name",
+    //   class: "charlie",
+    //   animations: "animate-name",
+    //   times: ".5"
+    // },
+    // {
+    //   tag: "span",
+    //   text: "Dear ",
+    //   parent: "#name"
+    // },
+    // {
+    //   tag: "span",
+    //   target: "name",
+    //   parent: "#name"
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'animate2__text',
+    //   parent: '#textAnimationBlock'
+    // },{
+    //   tag: 'div',
+    //   id: 'animate2__text-line1',
+    //   class: 'charlie display-none',
+    //   parent: '.animate2__text',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '5,8.6',
+    //   text: 'custId'
+    // },{
+    //   tag: 'div',
+    //   id: 'animate2__text-line2',
+    //   class: 'charlie display-none',
+    //   parent: '.animate2__text',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '5.2,8.6',
+    //   text: 'custId'
+    // },{
+    //   tag: 'div',
+    //   id: 'animate2__text-line3',
+    //   class: 'charlie display-none',
+    //   parent: '.animate2__text',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '5.4,8.6',
+    //   text: 'custId'
+    // },{
+    //   tag: 'div',
+    //   id: 'animate2__text-line4',
+    //   class: 'charlie',
+    //   parent: '.animate2__text',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '5.6,8.6',
+    //   target: 'custId'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'animate3',
+    //   parent: '#textAnimationBlock'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'charlie',
+    //   parent: '.animate3',
+    //   // animations: 'animate3__card1, animate3__card2 ,animate3-finish',
+    //   animations: 'animate3__card2 ,animate3-finish',
+    //   id: 'animate3__card',
+    //   // times: '9.7, 11,17.5'
+    //   times: '11,17.5'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'card__info',
+    //   parent: '#animate3__card'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'info__name',
+    //   parent: '.card__info',
+    //   target: 'card1Name'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'info__position',
+    //   parent: '.card__info'
+    // },
+    // {
+    //   tag: 'span',
+    //   text: 'Your ',
+    //   parent: '.info__position'
+    // },
+    // {
+    //   tag: 'span',
+    //   target: 'card1Position',
+    //   parent: '.info__position'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'info__number',
+    //   parent: '.card__info',
+    //   target: 'card1Number'
+    // },
+    // {
+    //   tag: 'div',
+    //   id: 'animate__line',
+    //   class: 'charlie',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '17.2,22.1'
+    // },
+    // {
+    //   tag: 'span',
+    //   text: 'In case you are unable to reach ',
+    //   parent: '#animate__line'
+    // },
+    // {
+    //   tag: 'span',
+    //   target: 'card1Firstname',
+    //   parent: '#animate__line'
+    // },
+    // {
+    //   tag: 'div',
+    //   class: 'animate4',
+    //   parent: '#textAnimationBlock'
+    // },
+    // {
+    //   tag: 'div',
+    //   id: 'animate4__name',
+    //   class: 'charlie',
+    //   parent: '.animate4',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '18.7,22.1',
+    //   target:'card2Name'
+    // },
+    // {
+    //   tag: 'div',
+    //   id: 'animate4__number',
+    //   class: 'charlie',
+    //   parent: '.animate4',
+    //   animations: 'animate-start,animate-finish',
+    //   times: '19.3,22.1',
+    //   target:'card2Number'
+    // },
     {
       tag: "a",
       id: "download-btn",
       class: "charlie show-end",
       animations: "hold",
-      times: "22.5",
+      times: "25",
       newWindow: "true"
     },
     {
       id: "cancel-btn",
       class: "charlie",
       animations: "hold",
-      times: "25.5"
+      times: "29"
     }
   ]
 };
