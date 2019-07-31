b0b3c43ee1c529dd54470d964366247d7dfc8d0f
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-07-31 12:36:25 +0530

    hdfc-glanza init

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,10 +1,64 @@
 var config = {
   // config here
+  videoUrl: "./app/img/glanza-empty.mp4",
+  startPoster: "./app/img/startPoster.jpg",
+  endPoster: "./app/img/endPoster.jpg",
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: "0.1",
+    },
+    {
+      text: "Dear ",
+      parent: "#name"
+    },
+    {
+      target: "name",
+      parent: "#name"
+    },
+    {
+      id: "loan",
+      class: "charlie",
+      animations: "animate-loan",
+      times: "4",
+    },
+    {
+      tag: "span",
+      text: "Click to avail your pre-approved car loan of ₹",
+      parent: "#loan"
+    },
+    {
+      tag: "span",
+      target: "loan",
+      parent: "#loan"
+    },
+    {
+      tag: "a",
+      id: "link",
+      class: "charlie show-end",
+      animations: "animate-link",
+      times: "4",
+      hrefTarget: "link",
+      newWindow: "true"
+    },
+    {
+      tag: "a",
+      id: "link1",
+      class: "charlie show-end",
+      animations: "animate-link",
+      times: "69.5",
+      hrefTarget: "link",
+      newWindow: "true"
+    }
+  ]
 };
 var pictor = new Pictor(config);
 
 pictor.init();
 
-// $(document).on('pictorInit', function() {
-//   // project specific functions should here
-// });
+$(document).on('pictorInit', function() {
+  // project specific functions should here
+  $("#pageBtn").attr("href", pictor.data.link);
+});
