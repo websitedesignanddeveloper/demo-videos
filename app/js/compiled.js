e2b6c53f5887abea9706c08d7f85b3c089594bff
ErHiteshSoni <34125048+ErHiteshSoni@users.noreply.github.com>
2019-08-05 19:52:05 +0530

    form inputs disabled

--- app/js/compiled.js
+++ app/js/compiled.js
@@ -3446,32 +3446,6 @@ pictor.init();
 
 $(document).on('pictorInit', function() {
   // project specific functions should here
-  // Restricts input for the given textbox to the given inputFilter.
-function setInputFilter(textbox, inputFilter) {
-  console.log("executed");
-  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
-    textbox.addEventListener(event, function() {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      }
-    });
-  });
-}
-
-// Restrict input to digits and '.' by using a regular expression filter.
-setInputFilter(document.getElementById("input-num"), function(value) {
-  return /^\d*$/.test(value);
-});
-
-setInputFilter(document.getElementById("input-pin"), function(value) {
-  return /^\d*$/.test(value);
-});
-
   $("#input-name").attr("value",pictor.data.name);
   $("#input-num").attr("value",pictor.data.number);
   $("#input-pin").attr("value",pictor.data.pin);
