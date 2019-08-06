a3d53da29c6c02f38a1e93a4b6fb4d8a7c5f8338
websitedesignanddeveloper <websitedesignanddeveloper@gmail.com>
2019-08-06 20:13:18 +0530

    Update config.js

--- app/dev/js/config.js
+++ app/dev/js/config.js
@@ -1,5 +1,15 @@
 var config = {
   // config here
+  videoUrl: "./app/img/glanza-empty.mp4",
+  startPoster: "./app/img/startPoster.jpg",
+  endPoster: "./app/img/endPoster.jpg",
+  rules: [
+    {
+      id: "name",
+      class: "charlie",
+      animations: "animate-name",
+      times: "0.1",
+    },
 };
 var pictor = new Pictor(config);
 
